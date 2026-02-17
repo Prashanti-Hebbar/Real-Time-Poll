6a07266b0025c2bf910fea39d37b176ffbfaec42
Prashanti-Hebbar <prashantihebbar344@gmail.com>
2026-02-17 16:59:10 +0530

    updated

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,7 +1,8 @@
-import { io } from 'socket.io-client';
-const socket = io('https://real-time-poll-tn0x.onrender.com',{
-    transports: ["websocket"],
-    withCredentials: true
+import { io } from "socket.io-client";
+
+const socket = io("https://real-time-poll-tn0x.onrender.com", {
+  transports: ["websocket"],
+  withCredentials: true
 });
 
 export default socket;
